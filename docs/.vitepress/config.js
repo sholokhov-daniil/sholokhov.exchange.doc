10892ab0dd6abf2f616134fbd545daadc0b52909
Шолохов Даниил <sholokhovdaniil@yandex.ru>
2025-10-18 23:21:21 +0700

    work(source): Изменение движка документации

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,75 @@
+// docs/.vitepress/config.js
+module.exports = {
+    // Относительные пути
+    base: '/',
+
+    // Включаем чистые URL (убираем .html и слэши в конце)
+    cleanUrls: true,
+    trailingSlash: false,
+
+    title: 'sholokhov.exchange',
+    description: 'Документация по модулю sholokhov.exchange',
+
+    head: [
+        ['link', {rel: 'icon', href: '/favicon.ico'}]
+    ],
+
+    themeConfig: {
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+        ],
+        sidebar: [
+            {
+                text: 'Начало работы',
+                items: [
+                    {text: 'Установка', link: '/'},
+                    {text: 'Конфигурация', link: '/guide/config'},
+                    {
+                        text: 'Карта обмена',
+                        link: '/guide/map/',
+                        items: [
+                            {text: 'Базовое свойство', link: '/guide/map/base'},
+                            {text: 'Информационный блок', link: '/guide/map/iblock'}
+                        ]
+                    },
+                    {text: 'Создание обмена', link: '/guide/start'}
+                ]
+            },
+            {
+                text: 'Источники данных',
+                items: [
+                    {text: 'Быстрый XML', link: '/source/xml-simple'},
+                    {text: 'Медленный XML', link: '/source/xml-db'},
+                    {text: 'Json', link: '/source/json'},
+                    {text: 'Json файл', link: '/source/json-file'},
+                ]
+            },
+            {
+                text: 'Импорт',
+                items: [
+                    {text: 'Файл', link: '/import/file'},
+                    {
+                        text: "Инфоблок",
+                        items: [
+                            {text: "Элемент", link: '/import/iblock/element'},
+                            {text: "Раздел", link: '/import/iblock/section'},
+                            {
+                                text: 'Свойства',
+                                items: [
+                                    {text: 'Значения списка', link: '/import/iblock/property/enum-value'}
+                                ]
+                            }
+                        ]
+                    },
+                    {text: "Элемент справочника", link: '/import/hl/element'},
+                    {
+                        text: 'Пользовательские свойства (UF)',
+                        items: [
+                            {text: 'Значение списка', link: '/import/uf/enum-value'}
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+}
--- src/data/codes/php/events/index.js
+++
@@ -1,9 +0,0 @@
-export default `
-use Bitrix\\Main\\EventManager;
-
-EventManager::getInstance()->addEventHandler(
-    'sholokhov.exchange',
-    'eventName',
-    $callback
-);
-`
